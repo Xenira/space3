2cd730eca44fc6a852dcadea98821fa221d16863
Xenira <1288524+Xenira@users.noreply.github.com>
2023-05-11 17:40:05 +0200

    Fixed database config

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t space3:wasm . && docker run --rm -it -e ROCKET_LOG_LEVEL=debug -e ROCKET_ADDRESS=0.0.0.0 -p 8000:8000 --link postgres space3:wasm
+docker build -t space3:wasm . && docker run --rm -it -e RUST_BACKTRACE=1 -e DATABASE_URL='postgres://postgres:mysecretpassword@postgres/postgres' -e ROCKET_LOG_LEVEL=debug -e ROCKET_ADDRESS=0.0.0.0 -p 8000:8000 --link postgres space3:wasm
